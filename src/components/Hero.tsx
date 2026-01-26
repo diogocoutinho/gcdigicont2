693575890e3c723913c74bfd4dffbfa6b6cceac2
Diogo C. Coutinho <diogo.coutinho.ads@gmail.com>
2026-01-26 15:50:45 -0300

    chore: Add .env*.local to .gitignore.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -29,7 +29,7 @@ export default function Hero() {
             fill
             className="object-cover"
             priority
-            quality={90}
+            quality={75}
             sizes="100vw"
           />
         </div>
